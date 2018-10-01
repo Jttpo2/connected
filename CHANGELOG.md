f8ce5a48d6540ff0d353be8e224801650cb30078
John Petersson <johnpetersson@gmail.com>
2018-10-01 19:59:38 +0200

    Initial

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/).
+
+## [Unreleased]
+
+## [0.3.0] - 2017-05-10
+### Added
+- Copying style.css into deployment folder
+
+### Changed
+- Better boilerplate.
+
+## [0.2.0] - 2017-04-29
+### Added
+- Concatenating js files into concat/concat.js using gulp-concant.
+
+### Changed
+- File structure.
+
+## [0.1.0] - 2017-04-18
+### Added
+- Basic setup: Node, Gulp, Livereload, gitignore, Readme, Changelog.
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp"),
+livereload = require('gulp-livereload'),
+concat = require("gulp-concat");
+
+var deploymentFolder = './deployment/';
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('**/*.css', ['reload', 'copy-css']);
+	gulp.watch('**/*.js', ['concat', 'reload']);
+	gulp.watch('**/*.html', ['reload']);
+	gulp.watch('**/README.md', ['copy-readme']);
+});
+
+gulp.task('reload', function() {
+	livereload.reload();
+});
+
+var sketchFolder = "sketch/";
+var sketchFiles = [
+	'particle.js',
+	'particle-handler.js',
+	'emptyp5project.js'
+];
+
+sketchFiles.forEach(function(fileName, index) {
+	sketchFiles[index] = sketchFolder + fileName;
+});
+
+gulp.task('concat', function() {
+	gulp.src(sketchFiles)
+	.pipe(concat('concat.js'))
+	.pipe(gulp.dest(deploymentFolder));
+});
+
+
+gulp.task('copy-css', function() {
+	gulp.src('style.css')
+	.pipe(gulp.dest(deploymentFolder));
+});
+
+gulp.task('copy-readme', function() {
+	gulp.src('README.md')
+	.pipe(gulp.dest(deploymentFolder));
+});
